f6772fe687ab278e48ff0166fe756c96cb984817
Frank Gehann <gehannf@gmail.com>
2021-05-03 22:56:25 +0200

    Added test for static sequence rollover.

--- tests/Identifier.Tests/SnowflakeIdTests.cs
+++ tests/Identifier.Tests/SnowflakeIdTests.cs
@@ -9,6 +9,7 @@ namespace Identifier.Tests
     {
         private readonly ITestOutputHelper _TestOutputHelper;
         private readonly int _GeneratorId = 5;
+        private readonly SnowflakeConfig _Config;
         private readonly SnowflakeId _Id;
 
         public SnowflakeIdTests(ITestOutputHelper testOutputHelper)
@@ -24,34 +25,34 @@ namespace Identifier.Tests
                 0,
                 DateTimeKind.Utc);
 
-            SnowflakeConfig config = new SnowflakeConfig(42, 6, 15, epoch);
+            _Config = new SnowflakeConfig(42, 6, 15, epoch);
 
-            _Id = new SnowflakeId(_GeneratorId, config);
+            _Id = new SnowflakeId(_GeneratorId, _Config);
             
             // Let's ask the config how many milliseconds we could use 
             // in this setup (42 bits)
-            _TestOutputHelper.WriteLine("Max. milliseconds     : {0}", config.MaxMilliseconds);
+            _TestOutputHelper.WriteLine("Max. milliseconds     : {0}", _Config.MaxMilliseconds);
             
             // Let's ask the config how many generators we could instantiate 
             // in this setup (6 bits)
-            _TestOutputHelper.WriteLine("Max. generators       : {0}", config.MaxGenerators);
+            _TestOutputHelper.WriteLine("Max. generators       : {0}", _Config.MaxGenerators);
 
             // Let's ask the config how many sequential Id's we could generate 
             // in a single ms in this setup (15 bits)
-            _TestOutputHelper.WriteLine("Id's/ms per generator : {0}", config.MaxSequenceIds);
+            _TestOutputHelper.WriteLine("Id's/ms per generator : {0}", _Config.MaxSequenceIds);
 
             // Let's calculate the number of Id's we could generate, per ms, should we use
             // the maximum number of generators
-            _TestOutputHelper.WriteLine("Id's/ms total         : {0}", config.MaxGenerators * config.MaxSequenceIds);
+            _TestOutputHelper.WriteLine("Id's/ms total         : {0}", _Config.MaxGenerators * _Config.MaxSequenceIds);
 
 
             // Let's ask the config configuration for how long we could generate Id's before
             // we experience a 'wraparound' of the timestamp
-            _TestOutputHelper.WriteLine("Wraparound interval   : {0}", config.WraparoundInterval());
+            _TestOutputHelper.WriteLine("Wraparound interval   : {0}", _Config.WraparoundInterval());
 
             // And finally: let's ask the config when this wraparound will happen
             // (we'll have to tell it the generator's epoch)
-            _TestOutputHelper.WriteLine("Wraparound date       : {0}", config.WraparoundDate());
+            _TestOutputHelper.WriteLine("Wraparound date       : {0}", _Config.WraparoundDate());
         }
 
         [Fact]
@@ -81,6 +82,32 @@ namespace Identifier.Tests
             
             Assert.Equal(time, decodedTime);
             Assert.Equal(_GeneratorId, generator);
+        }
+        
+        [Fact]
+        public void TestStaticSequenceRollover()
+        {
+            SnowflakeId idGen = new SnowflakeId(0, _Config);
+
+            DateTime time = new DateTime(
+                2000,
+                1,
+                1,
+                0,
+                0,
+                0,
+                DateTimeKind.Utc); 
+            
+            long maxSequence = _Config.MaxSequenceIds;
+            while (maxSequence > 0)
+            {
+                idGen.NewId(time);
+                maxSequence--;
+            }
+            
+            long id = idGen.NewId();
+            int sequence = idGen.GetSequence(id);
+            Assert.Equal(0, sequence);
         }
     }
 }
